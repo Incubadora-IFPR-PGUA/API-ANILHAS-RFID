cb0f48a876f8ca1a0ddd71a54c7eb411e47de1b4
mauboru <josue21henrique@gmail.com>
2024-09-08 15:40:08 -0300

    update code

---
+++ src/models/AnilhaCadastrada.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../instances/mysql";
+
+export class AnilhaCadastrada extends Model {
+  public id!: number;
+  public name!: string;
+  public codigo!: string;
+  public readonly created_at!: Date;
+  public readonly updated_at!: Date;
+}
+
+AnilhaCadastrada.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    codigo: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null,
+    },
+  },
+  {
+    sequelize,
+    tableName: "anilhas_cadastros",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,10 +5,17 @@ import * as AnilhaController from "../controllers/AnilhaController";
 
 const router = Router();
 
-router.get("/listarAnilha", AnilhaController.listarAnilha);
 router.post("/inserirAnilha", AnilhaController.inserirAnilha);
-router.get("/listarAnilha/:idAnilha", AnilhaController.getAnilhaById); 
-router.delete("/excluirAnilha/:idAnilha", AnilhaController.excluirAnilha);
-router.put("/atualizarAnilha/:idAnilha", AnilhaController.atualizarAnilha);
+router.get("/listarAnilhaCadastradas", AnilhaController.listarAnilhaCadastradas);
+router.get("/listarAnilhaPendentes", AnilhaController.listarAnilhaPendentes);
+router.get("/listarAnilhaRegistros", AnilhaController.listarAnilhaRegistros);
+router.put("/atualizarAnilhaCadastrada/:idAnilha", AnilhaController.atualizarAnilhaCadastrada);
+router.put("/atualizarAnilhaPendente/:idAnilha", AnilhaController.atualizarAnilhaPendente);
+router.get("/listarAnilhaCadastradas/:idAnilha", AnilhaController.getAnilhaCadastradaById); 
+router.get("/listarAnilhaPendentes/:idAnilha", AnilhaController.getAnilhaPendenteById); 
+router.get("/listarAnilhaRegistros/:idAnilha", AnilhaController.getAnilhaRegistroById); 
+router.delete("/excluirAnilhaCadastrada/:idAnilha", AnilhaController.excluirAnilhaCadastrada);
+router.delete("/excluirAnilhaPendente/:idAnilha", AnilhaController.excluirAnilhaPendente);
+router.delete("/excluirAnilhaRegistro/:idAnilha", AnilhaController.excluirAnilhaRegistro);
 
 export default router;
