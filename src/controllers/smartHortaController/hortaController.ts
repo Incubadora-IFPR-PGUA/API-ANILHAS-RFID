a6de73a01938214450614cda3bedcfc16aff3b9b
mauboru <josue21henrique@gmail.com>
2024-09-25 14:24:10 -0300

    order na listHorta

--- src/controllers/smartHortaController/hortaController.ts
+++ src/controllers/smartHortaController/hortaController.ts
@@ -51,7 +51,9 @@ export const atualizarHorta = async (req: Request, res: Response) => {
 // Listar todos os registros da horta
 export const listarHorta = async (req: Request, res: Response) => {
   try {
-    const registros = await HortaModel.findAll();
+    const registros = await HortaModel.findAll({
+      order: [["hora_atualizacao", "DESC"]],
+    });
     res.status(200).json({ data: registros });
   } catch (error) {
     console.error("Erro ao listar registros da horta:", error);
