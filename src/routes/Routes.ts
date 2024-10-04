ee9c007a43e38f43e536e08f146b0e81db82faf9
Kayure <luccas_kayure@hotmail.com>
2024-10-04 18:40:21 -0300

    API PhMetro

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -7,6 +7,7 @@ import * as AnilhaRegistroController from "../controllers/anilhaController/anilh
 import * as EspMacAdressController from "../controllers/macAdressController/espMacAdressController";
 import * as MacsCapturadosController from "../controllers/macAdressController/macsCapturadosController";
 import * as HortaController from "../controllers/smartHortaController/hortaController";
+import * as PhMetroController from "../controllers/phMetroController/phMetroController";
 
 const router = Router();
 
@@ -37,8 +38,8 @@ router.delete("/deletarEsp/:id", EspMacAdressController.deletarEsp);
 
 //MAC ADRESS (MAC)
 router.post("/inserirMacCapturado", MacsCapturadosController.inserirMacCapturado);
-router.get("/listarMacsCapturados", MacsCapturadosController.listarMacsCapturados); 
-router.get("/obterMacPorId/:id", MacsCapturadosController.obterMacPorId); 
+router.get("/listarMacsCapturados", MacsCapturadosController.listarMacsCapturados);
+router.get("/obterMacPorId/:id", MacsCapturadosController.obterMacPorId);
 router.put("/atualizarMacCapturado/:id", MacsCapturadosController.atualizarMacCapturado);
 router.delete("/deletarMacCapturado/:id", MacsCapturadosController.deletarMacCapturado);
 
@@ -46,7 +47,14 @@ router.delete("/deletarMacCapturado/:id", MacsCapturadosController.deletarMacCap
 router.post("/inserirHorta", HortaController.inserirHorta);
 router.put("/atualizarHorta/:id", HortaController.atualizarHorta);
 router.get("/listarHorta", HortaController.listarHorta);
-router.get("/obterHortaPorId/:id", HortaController.obterHortaPorId); 
+router.get("/obterHortaPorId/:id", HortaController.obterHortaPorId);
 router.delete("/deletarHorta/:id", HortaController.deletarHorta);
+
+// PHMETRO
+router.post("/inserirPh", PhMetroController.criarPh);
+router.put("/atualizarPh/:id", PhMetroController.atualizarPh);
+router.get("/listarPh", PhMetroController.listarPhs);
+router.get("/obterPhPorId/:id", PhMetroController.buscarPhPorId);
+router.delete("/deletarPh/:id", PhMetroController.deletarPh);
 
 export default router;
