928ce4ae22d9562f9954b2e154c5de00214bc911
Beatriz Yoshimi Yukizaki <yoshimidev1@gmail.com>
2026-01-23 15:45:40 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "api-vitrine",
-      script: "dist/index.js", 
-      instances: "max",       
-      exec_mode: "cluster",
-
+      name: "API-VITRINE",
+      script: "./dist/server.js",
+      instances: 1,
       autorestart: true,
       watch: false,
+      interpreter: "node",
 
+      max_restarts: 50,
+      min_uptime: 5000,
       max_memory_restart: "300M",
 
       env: {
-        NODE_ENV: "development",
-        PORT: 3000
-      },
-
-      env_production: {
         NODE_ENV: "production",
-        PORT: 3000
       },
 
-      error_file: "./logs/err.log",
-      out_file: "./logs/out.log",
-      log_file: "./logs/combined.log",
-      time: true
-    }
-  ]
+      error_file: "logs/error.log",
+      out_file: "logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
+  ],
 };
